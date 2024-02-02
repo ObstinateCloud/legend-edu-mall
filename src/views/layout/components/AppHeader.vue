865bdcce66ea1b8649700ef328578adb6975fafe
zhangjianyun <zhangjianyun_kd@163.com>
2024-02-02 17:12:49 +0800

    增加登出和头像

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="app-header">
     <i :class="iconClassName" @click="toggleCollapse"></i>
-    <el-dropdown>
-      <i class="el-icon-setting" style="margin-left:50px"></i>
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>查看</el-dropdown-item>
-        <el-dropdown-item>新增</el-dropdown-item>
-        <el-dropdown-item>删除</el-dropdown-item>
-      </el-dropdown-menu>
-    </el-dropdown>
-    <span>传奇</span>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
+  <el-dropdown>
+  <span class="el-dropdown-link">
+    <el-avatar :size="40" :src="circleUrl"></el-avatar><i class="el-icon-arrow-down el-icon--right"></i>
+  </span>
+  <el-dropdown-menu slot="dropdown">
+    <el-dropdown-item divided>个人信息</el-dropdown-item>
+    <el-dropdown-item divided>登出</el-dropdown-item>
+  </el-dropdown-menu>
+</el-dropdown>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AppHeader',
+  data () {
+    return {
+      circleUrl: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+    }
+  },
   props: {
     isCollapse: {
       type: Boolean,
@@ -36,6 +47,18 @@ export default {
 </script>
 <style lang="scss" scoped>
 .app-header {
-  text-align: left;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  .el-breadcrumb {
+    margin-left: 10px;
+  }
+  .el-dropdown {
+    margin-left: auto; //会在最右面
+  }
+  .el-dropdown-link {
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
